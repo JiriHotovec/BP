44f47e2d348415540cfaa75872a30eec1fece609
Jiri Hotovec <hotovec@outlook.cz>
2025-02-26 23:08:52 +0100

    feat: add handlers interfaces

---
+++ src/Hotovec.Orders.Application/UseCases/Common/ICommandHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace Hotovec.Orders.Application.UseCases.Common;
+
+public interface ICommandHandler<in T>
+{
+    Task ExecuteAsync(T command, CancellationToken cancellationToken = default);
+}
---
+++ src/Hotovec.Orders.Application/UseCases/Common/IQueryHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace Hotovec.Orders.Application.UseCases.Common;
+
+public interface IQueryHandler<in TInput,TOutput>
+{
+    Task<TOutput> ExecuteQueryAsync(TInput input, CancellationToken cancellationToken = default);
+}
