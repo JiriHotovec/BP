757bd30b61cea3b79491846d6f8a951c198ad2a6
Jiri Hotovec <hotovec@outlook.cz>
2025-02-26 22:26:48 +0100

    feat: add architecture test

---
+++ test/Hotovec.Orders.Test/Architecture/ArchitecturalTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+
+namespace Hotovec.Orders.Test.Architecture;
+
+public class ArchitecturalTests
+{
+    [Fact]
+    public void Interfaces_WithIPrefix_ReturnsTrue()
+    {
+        // Arrange
+        string[] assemblyNames = [
+            "Hotovec.Orders.Api",
+            "Hotovec.Orders.Application",
+            "Hotovec.Orders.Domain",
+            "Hotovec.Orders.Infrastructure",
+        ];
+        var assemblies = assemblyNames.Select(Assembly.Load);
+        
+        // Act
+        var result = Types
+            .InAssemblies(assemblies)
+            .That()
+            .AreInterfaces()
+            .Should()
+            .HaveNameStartingWith("I")
+            .GetResult();
+
+        // Assert
+        Assert.True(result.IsSuccessful);
+    }
+}
