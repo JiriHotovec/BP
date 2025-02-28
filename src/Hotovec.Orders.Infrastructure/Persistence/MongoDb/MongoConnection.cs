3b4332aa7e7ca6d112df65cbaa1e7b9926395d1a
Jiri Hotovec <hotovec@outlook.cz>
2025-02-28 19:16:31 +0100

    fix: add MongoDB connection string

--- src/Hotovec.Orders.Infrastructure/Persistence/MongoDb/MongoConnection.cs
+++ src/Hotovec.Orders.Infrastructure/Persistence/MongoDb/MongoConnection.cs
@@ -5,13 +5,18 @@ namespace Hotovec.Orders.Infrastructure.Persistence.MongoDb;
 
 internal sealed class MongoConnection : IDisposable, IMongoConnection
 {
+    private readonly TimeSpan _connectionTimeout = TimeSpan.FromSeconds(10);
+    private readonly TimeSpan _serverSelectionTimeout = TimeSpan.FromSeconds(3);
     private readonly MongoClient _client;
     private readonly IMongoDatabase _database;
 
     public MongoConnection(IOptions<MongoOptions> mongoOptions)
     {
         var options = mongoOptions.Value;
-        _client = new MongoClient(options.ConnectionString);
+        var mongoSettings = MongoClientSettings.FromConnectionString(options.ConnectionString);
+        mongoSettings.ConnectTimeout = _connectionTimeout;
+        mongoSettings.ServerSelectionTimeout = _serverSelectionTimeout;
+        _client = new MongoClient(mongoSettings);
         _database = _client.GetDatabase(options.DatabaseName);
     }
     
