44f1db255cfb5b608f19791e666d4065a71d0e95
Jiri Hotovec <hotovec@outlook.cz>
2025-03-04 20:35:39 +0100

    feat: add ConsoleApp for tests

---
+++ src/Hotovec.Orders.Performance.test.ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using Hotovec.Orders.Domain.Orders.MonetaryInformation;
+
+Console.WriteLine("Set performance counters!");
+
+while (true)
+{
+    var money = new Money(1.1m, new Currency("USD"));
+    money = null;
+}
