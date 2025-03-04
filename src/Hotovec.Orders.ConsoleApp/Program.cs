c072cc3dda30563faf29e10247ec7b0e7031ee1a
Jiri Hotovec <hotovec@outlook.cz>
2025-03-04 01:22:18 +0100

    feat: add BenchmarkDotNet tests

---
+++ src/Hotovec.Orders.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+using Hotovec.Orders.Test.Performance.Benchmark;
+
+Console.WriteLine("Benchmark Tests");
+
+var config = new ManualConfig()
+    .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+    .AddValidator(JitOptimizationsValidator.DontFailOnError)
+    .AddLogger(ConsoleLogger.Default)
+    .AddColumnProvider(DefaultColumnProviders.Instance);
+
+BenchmarkRunner.Run<MoneyTests>(config);
