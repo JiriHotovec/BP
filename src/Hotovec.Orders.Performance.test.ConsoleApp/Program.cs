61b31018e68da4b7885a5c1b22fdb0af7c228342
Jiri Hotovec <hotovec@outlook.cz>
2025-03-13 01:40:07 +0100

    feat: remove performance test projects

--- src/Hotovec.Orders.Performance.test.ConsoleApp/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Hotovec.Orders.Domain.Orders.MonetaryInformation;
-
-Console.WriteLine("Set performance counters!");
-
-while (true)
-{
-    var money = new Money(1.1m, new Currency("USD"));
-    money = null;
-}
