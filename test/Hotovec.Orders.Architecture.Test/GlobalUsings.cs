85ad97d603687a8b551ce74af19bb4a34b1c8978
Jiri Hotovec <hotovec@outlook.cz>
2025-03-13 01:57:11 +0100

    feat: move architecture tests

---
+++ test/Hotovec.Orders.Architecture.Test/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using FluentAssertions;
+global using NetArchTest.Rules;
+global using System.ComponentModel;
+global using Xunit;
---
+++ test/Hotovec.Orders.Architecture.Test/NamingConventionTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection;
+using Hotovec.Orders.Domain.Common.Entities;
+
+namespace Hotovec.Orders.Architecture.Test;
+
+[Category("architecture")]
+public sealed class NamingConventionTests
+{
+    [Fact]
+    public void Interfaces_WithIPrefix_ReturnsTrue()
+    {
+        // Arrange
+        string[] assemblyNames = [
+            AssemblyConstants.ApiAssemblyName,
+            AssemblyConstants.ApplicationAssemblyName,
+            AssemblyConstants.DomainAssemblyName,
+            AssemblyConstants.InfrastructureAssemblyName,
+        ];
+        var assemblies = assemblyNames.Select(Assembly.Load);
+        
+        // Act
+        var actual = Types
+            .InAssemblies(assemblies)
+            .That()
+            .AreInterfaces()
+            .Should()
+            .HaveNameStartingWith("I")
+            .GetResult();
+
+        // Assert
+        actual.IsSuccessful.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void Entities_WithEntityPostfix_ReturnsTrue()
+    {
+        // Arrange
+        string[] assemblyNames = [
+            AssemblyConstants.DomainAssemblyName
+        ];
+        var assemblies = assemblyNames.Select(Assembly.Load);
+        
+        // Act
+        var actual = Types
+            .InAssemblies(assemblies)
+            .That()
+            .Inherit(typeof(Entity<,>))
+            .Should()
+            .HaveNameEndingWith("Entity")
+            .GetResult();
+
+        // Assert
+        actual.IsSuccessful.Should().BeTrue();
+    }
+}
