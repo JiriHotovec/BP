b23467363364c5647aaf0a1220e55fa75ea304cf
Jiri Hotovec <hotovec@outlook.cz>
2025-03-13 23:32:01 +0100

    refactor: remove unused ctors

--- src/Hotovec.Orders.Application/Exceptions/ApplicationException.cs
+++ src/Hotovec.Orders.Application/Exceptions/ApplicationException.cs
@@ -2,15 +2,7 @@
 
 public class ApplicationException : Exception
 {
-    public ApplicationException()
-    {
-    }
-
     public ApplicationException(string message) : base(message)
-    {
-    }
-
-    public ApplicationException(string message, Exception inner) : base(message, inner)
     {
     }
 }
