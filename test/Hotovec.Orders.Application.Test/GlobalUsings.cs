8356cf50360fa7bb3a142d3194e2b1c208b86756
Jiri Hotovec <hotovec@outlook.cz>
2025-03-13 23:53:15 +0100

    feat: add CreateOrderCommandHandlerTests

---
+++ test/Hotovec.Orders.Application.Test/GlobalUsings.cs
@@ -0,0 +1,3 @@
+global using FluentAssertions;
+global using System.ComponentModel;
+global using Xunit;
---
+++ test/Hotovec.Orders.Application.Test/UseCases/Commands/CreateOrder/CreateOrderCommandHandlerTests.cs
@@ -0,0 +1,33 @@
+﻿using Hotovec.Orders.Application.Persistence;
+using Hotovec.Orders.Application.UseCases.Commands.CreateOrder;
+using Hotovec.Orders.Domain.Orders;
+using Hotovec.Orders.Domain.Orders.MonetaryInformation;
+using NSubstitute;
+using ApplicationException = Hotovec.Orders.Application.Exceptions.ApplicationException;
+
+namespace Hotovec.Orders.Application.Test.UseCases.Commands.CreateOrder;
+
+public class CreateOrderCommandHandlerTests
+{
+    [Fact]
+    public void ExecuteAsync_AlreadyExistedOrder_ThrowsApplicationException()
+    {
+        // Arrange
+        var mRepository = Substitute.For<IOrderEntityRepository>();
+        mRepository
+            .ExistsAsync(Arg.Any<OrderNumber>())
+            .Returns(true);
+        var mFactory = Substitute.For<ICreateOrderCommandFactory>();
+        var orderNumber = new OrderNumber("ORDER_10");
+        var currency = new Currency("CZK");
+        var command = new CreateOrderCommand(orderNumber, currency, "Jim Slim", [
+            new CreateOrderItem(1, "Item 1", 1000, new Money(1000, currency)) ]);
+        var handler = new CreateOrderCommandHandler(mRepository, mFactory);
+        
+        // Act
+        var actual = async () => await handler.ExecuteAsync(command);
+        
+        // Assert
+        actual.Should().ThrowAsync<ApplicationException>();
+    }
+}
